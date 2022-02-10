bd21f03cd92c894e9595776eb443e8c7c09a568f
Vincent0Lee <vincent0lee95@gmail.com>
2022-02-10 20:34:27 +0800

    Connect to react app using proxy or linking

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,30 @@
 import express from 'express';
+import path from 'path';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    // absolute path to local-client in package
+    const pkgPath = require.resolve('local-client/build/index.html');
+    // Naive solution, won't work: can't reference files between different pkg
+    // So we need to use lerna to link local-client to local-api
+    app.use(express.static(path.dirname(pkgPath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
